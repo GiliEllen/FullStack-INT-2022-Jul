4b6d4d7bd9879df2965fa9b3c91c3261400d97a1
Gili <gilimena95@gmail.com>
2022-12-15 19:36:08 +0200

    WIP

---
+++ Node.js/01-basic/example2/server.ts
@@ -0,0 +1,32 @@
+import express from "express"; 
+import fs from 'fs';
+
+const app = express();
+const port = 4000;
+
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInput);
+
+app.get("/", (req:express.Request, res:express.Response) => {
+    res.send(textInput)
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`)
+})
+
+// const day = new Date().getDay();
+// const month = new Date().getMonth();
+// const today = `${day}.${month}`
+
+// const textOutput = `this is what we know about node.js: ${textInput} \n created on ${today}`
+
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("file was created");
+
+// let name = "gili"
+// const text = "hi my name is " + name + "!";
+// const text2 = `hi my name is ${name}!`;
+
+// `abcde ${var}!`
+// console.log(text2)
