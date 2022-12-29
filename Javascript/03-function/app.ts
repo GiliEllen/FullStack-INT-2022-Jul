bf11766709f85d86b7c3a115e604dd1920109895
gili <gilimena95@gmail.com>
2022-12-29 20:33:50 +0200

    WIP

---
+++ Javascript/03-function/app.ts
@@ -0,0 +1,7 @@
+
+function multy(a,b) {
+    const result = a*b
+    return result
+}
+
+console.log(multy(2,2))
---
+++ Javascript/03-function/dist/app.js
@@ -0,0 +1,5 @@
+function multy(a, b) {
+    var result = a * b;
+    return result;
+}
+console.log(multy(2, 2));
