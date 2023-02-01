54456e54da695392e303ad500db6869aa8d1ac99
Gili <gilimena95@gmail.com>
2023-02-01 19:31:10 +0200

    WiP

---
+++ Node.js/08-JWT-ans-local-storage/example/public/app.ts
@@ -0,0 +1,112 @@
+async function handleLogin(ev: any) {
+  try {
+    ev.preventDefault();
+    const password = ev.target.elements.passwordLog.value;
+    const email = ev.target.elements.emailLog.value;
+    console.log(password, email);
+
+    //@ts-ignore
+    const { data } = await axios.post("/api/users/login", { password, email });
+    console.log(data);
+    const { ok } = data;
+    if (ok) {
+      console.log("suuccesful Login");
+      window.location.href = "./home.html";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleRegister(ev: any) {
+  try {
+    ev.preventDefault();
+    const password = ev.target.elements.password.value;
+    const email = ev.target.elements.email.value;
+    const repeatPassword = ev.target.elements.repeatPassword.value;
+    const username = ev.target.elements.username.value;
+    if (!email || !password || !username || !repeatPassword)
+      throw new Error("password and email is required");
+
+    //@ts-ignore
+    const { data } = await axios.post("/api/users/register", {
+      password,
+      email,
+      repeatPassword,
+      username,
+    });
+    console.log(data);
+    const { register, error } = data;
+
+    if (error) throw error;
+    if (register) window.location.href = "./home.html";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getUserFromCookie() {
+  try {
+    //@ts-ignore
+    const { data } = await axios.get("/api/users/get-user-by-cookie");
+    const { userDB } = data;
+    const username = document.querySelector("#username");
+    username.innerHTML = `${userDB.email}`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleCheckIfUserIsconnected() {
+  try {
+    //@ts-ignore
+    const { data } = await axios.get("/api/users/get-user-by-cookie");
+    const { userDB } = data;
+    if (userDB) window.location.href = "./home.html";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleLogout() {
+  try {
+    //@ts-ignore
+    const { data } = await axios.get("/api/users/logout");
+    const { logout } = data;
+    if (logout) window.location.href = "./index.html";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handlegetAllUsers() {
+  try {
+    //@ts-ignore
+    const { data } = await axios.get("/api/users");
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getUserById(event) {
+  try {
+    event.preventDefault();
+    const userId = event.target.elements.userId.value;
+    //@ts-ignore
+    const { data } = await axios.get(`/api/users/${userId}`);
+    console.log(data);
+  } catch (error) {}
+}
+
+async function handleUpdateUser(event) {
+  try {
+    const password = event.target.elements.password.value;
+    const userId = event.target.elements.userId.value;
+    //@ts-ignore
+    const { data } = await axios.patch(`/api/users/${userId}`, { password });
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
