8b9a8a756963f01326bdfd0de9d1d50a217c2f76
Gili <gilimena95@gmail.com>
2023-02-05 17:27:17 +0200

    WIP

--- Debugging-And-Error-Handling/01-vsc-debugger/app.js
+++ Debugging-And-Error-Handling/01-vsc-debugger/app.js
@@ -1,9 +1,19 @@
-function helloWorld() {
-    sayHello()
-}
+const measureKelvin = function () {
+    const measurement = {
+      type: 'temp',
+      unit: 'celsius',
+      // C) FIX
+    //   value: Number(prompt('Degrees celsius:')),
+      value: prompt('Degrees celsius:'),
+    //   value: 10,
+    };
+    // B) FIND
+    console.table(measurement);
+    // console.log(measurement.value);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+    const kelvin = measurement.value + 273;
+    return kelvin;
+  };
 
-function sayHello() {
-    console.log("hello world")
-}
-
-helloWorld()
+  console.log(measureKelvin());
