9e5384c53da6839187dde83f1c5fda3feec3847d
Gili <gilimena95@gmail.com>
2023-02-06 18:59:31 +0200

    Update app.js
    added number function to update value of measurement

--- Debugging-And-Error-Handling/01-vsc-debugger/app.js
+++ Debugging-And-Error-Handling/01-vsc-debugger/app.js
@@ -2,11 +2,11 @@ const measureKelvin = function () {
     const measurement = {
       type: 'temp',
       unit: 'celsius',
-    //   value: Number(prompt('Degrees celsius:')),
-      value: prompt('Degrees celsius:'),
+      value: Number(prompt('Degrees celsius:')),
+      // value: prompt('Degrees celsius:'),
     };
     console.table(measurement);
-    // console.log(measurement.value);
+    console.log(measurement);
     // console.warn(measurement.value);
     // console.error(measurement.value);
     const kelvin = measurement.value + 273;
