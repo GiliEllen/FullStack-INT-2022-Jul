a58cb2bd87b9b3719a583482bb20f949150c9bc5
gili <gilimena95@gmail.com>
2023-02-09 18:17:02 +0200

    WIP

---
+++ Node.js/99-projectsHelp/albert/API/cars/carsModel.ts
@@ -0,0 +1,42 @@
+
+// //id = 1
+// carName: "mazda lantis"
+// carYear: 2014
+// carImage: URL()
+// color: gray
+// userId: undefiend
+
+// //id = 2
+// carName: "mazda lantis"
+// carYear: 2014
+// carImage: URL()
+// color: black
+// userId: safkgvkdbv264v5fsvszfv
+
+// //RentCar 
+// // 1. check if user rented a car - check cars collection if userId exist
+// //2. edit car document to have userId
+// //
+// //[{},{}]
+
+// // if(!car.userId) = add div
+// // if(car.userId) = don't add div / add div with disabled rent button
+
+// // <div class="car_container">
+// // <img src`${image from array}`/>
+// // </div>
+
+// //option 2
+// //id = 1
+// carName: "mazda lantis"
+// carYear: 2014
+// availableCars:100
+// carImage: URL()
+// color: gray
+
+// //id = 2
+// carName: "pegout 307"
+// carYear: 2004
+// availableCars: 500
+// carImage: URL()
+// color: black
---
+++ Node.js/99-projectsHelp/albert/API/users/userModel.ts
@@ -0,0 +1,26 @@
+// //option 1
+
+// email: {
+//     type: string
+// },
+// firstName: {
+//     type: string
+// },
+// password: {
+//     type:string
+// }
+// role: user / admin
+
+// //option 2
+
+// email: {
+//     type: string
+// },
+// firstName: {
+//     type: string
+// },
+// password: {
+//     type:string
+// }
+// role: user/admin
+// rentedCar: carId //undefiend
