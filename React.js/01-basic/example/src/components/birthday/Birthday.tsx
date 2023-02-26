eb20ab619922e8200d8a636fba3962ed0077bf3d
Gili <gilimena95@gmail.com>
2023-02-26 20:52:02 +0200

    WIP

--- React.js/01-basic/example/src/components/birthday/Birthday.tsx
+++ React.js/01-basic/example/src/components/birthday/Birthday.tsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
+// function Birthday() {
+//     const currentYear = new Date().getFullYear()
+//     const [age, setAge] = useState<number| string>("press button please")
+
+//     function handleBirthday() {
+//         const birthday = prompt("enter birth year")
+//         if (! birthday) {
+//             setAge("please press button")
+//         } else {
+//             const ageCalc = Number(currentYear) - Number(birthday)
+//             setAge(ageCalc)
+//         }
+//     }
+
+//   return (
+//     <div>
+//         <button onClick={handleBirthday}>Calculate</button>
+//         <div>{age}</div>
+//     </div>
+//   )
+// }
+
+// export default Birthday
+
+const currentYear = new Date().getFullYear();
 function Birthday() {
-    const currentYear = new Date().getFullYear()
-    const [age, setAge] = useState<number| string>("press button please")
+  const [age, setAge] = useState<number | string>(currentYear);
 
-    function handleBirthday() {
-        const birthday = prompt("enter birth year")
-        if (! birthday) {
-            setAge("please press button")
-        } else {
-            const ageCalc = Number(currentYear) - Number(birthday)
-            setAge(ageCalc)
-        }
+  function handleBirthday() {
+    const birthday = prompt("enter birth year");
+    if (!birthday) {
+      setAge("please press button");
+    } else {
+      setAge(Number(age) - Number(birthday));
     }
-
+  }
   return (
     <div>
-        <button onClick={handleBirthday}>Calculate</button>
         <div>{age}</div>
+      <button onClick={handleBirthday}></button>
     </div>
-  )
+  );
 }
 
-export default Birthday
+export default Birthday;
