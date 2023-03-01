046aed03ac3c14924416ce5f8ee910d681ace0f6
Gili <gilimena95@gmail.com>
2023-03-01 10:42:23 +0200

    WIP

--- React.js/99-practice/28-02/task01/src/components/square/Sqaure.tsx
+++ React.js/99-practice/28-02/task01/src/components/square/Sqaure.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 const Sqaure = () => {
     const [backgroundColorofSqaure, setBackgroundColorofSqaure] = useState<string>('#8e4b4b')
+    const [age, setAge] = useState<number>()
+
 
     const colors = ['red', 'blue', 'black', 'rgb(255,255,0)']
     function handleChangeColour() {
@@ -18,8 +20,17 @@ const Sqaure = () => {
         setBackgroundColorofSqaure(`rgb(${getRandom(0,255)}, ${getRandom(0,255)}, ${getRandom(0,255)})`)
     }
 
+    function handleSetAge(event:any) {
+        const year = Number(prompt('enter birth year'))
+        const ageOfUser = new Date().getFullYear() - year;
+        setAge(ageOfUser)
+    }
+
   return (
-    <div className='sqaure' style={{backgroundColor: backgroundColorofSqaure}} onClick={harderColors}>Sqaure</div>
+    <div className='sqaure' style={{backgroundColor: backgroundColorofSqaure}} onClick={harderColors}>
+        <button onClick={handleSetAge}>XXX</button>
+        <p>Your age is: {age}</p>
+    </div>
   )
 }
 
