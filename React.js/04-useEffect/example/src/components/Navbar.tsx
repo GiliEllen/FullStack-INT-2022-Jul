d8f0940c2da568ca64e8d22ddb6b73e13b4c3972
gili <gilimena95@gmail.com>
2023-03-02 18:27:43 +0200

    WIP

--- React.js/04-useEffect/example/src/components/Navbar.tsx
+++ React.js/04-useEffect/example/src/components/Navbar.tsx
@@ -1,9 +1,28 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Navbar = () => {
   const [resourceType, setResourceType] = useState<string>("posts");
-  
+  const [items, setItems] = useState<object[]>([])
+  //useEffect
+  console.log("render");
+  async function handleGetResource() {
+    try {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resourceType}`
+      );
+      console.log(data);
+      setItems(data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("on resource change");
+    handleGetResource()
+  }, []);
+
   return (
     <>
       <div>
@@ -12,6 +31,9 @@ const Navbar = () => {
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <p> {JSON.stringify(item)}</p>
+      })} 
     </>
   );
 };
