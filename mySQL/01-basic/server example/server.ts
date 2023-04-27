0872b5cf58f16fbbd85adb3eb1b1aee5ee32371a
Gili <gilimena95@gmail.com>
2023-04-27 17:47:38 +0300

    WIP

--- mySQL/01-basic/server example/server.ts
+++ mySQL/01-basic/server example/server.ts
@@ -10,6 +10,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("client"));
 
 app.post("/api/create-database", (req, res) => {
   const query = "CREATE DATABASE gili;";
@@ -17,6 +18,21 @@ app.post("/api/create-database", (req, res) => {
     try {
       if (err) throw err;
       console.log(results);
+      res.send({ results, ok: true });
+    } catch (error) {
+      console.log(error);
+      res.send({ ok: false, error: error.message });
+    }
+  });
+});
+app.post("/api/inser-into-movies", (req, res) => {
+  const { title, year, runtime, director, actors, plot, posterUrl } = req.body;
+  console.log(title, year, runtime, director, actors, plot, posterUrl);
+  const query = `INSERT INTO blockbuster.movies (title, year, runtime, director, actors, plot, posterUrl) VALUES ("${title}", ${year}, ${runtime}, "${director}", "${actors}", "${plot}", "${posterUrl}");`;
+  connection.query(query, (err, results, fields) => {
+    try {
+      if (err) throw err;
+      // console.log(results);
       res.send({ results, ok: true });
     } catch (error) {
       console.log(error);
