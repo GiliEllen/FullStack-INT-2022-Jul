426e7c889c103cce114a01f36951d2e3836de0a3
Gili <gilimena95@gmail.com>
2023-06-06 11:54:44 +0300

    WIP

--- React.js/99-practice/debugging/baseProject/src/components/index.tsx
+++ React.js/99-practice/debugging/baseProject/src/components/index.tsx
@@ -17,8 +17,6 @@ const Container = styled.div`
 export function HomePage(props: IHomePageProps) {
   const [movies, setMovies] = useState([]);
   const [delay, setDelay] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<any>(null)
 
   const options = {
     method: "GET",
@@ -44,34 +42,26 @@ export function HomePage(props: IHomePageProps) {
 
   const handleGetMovies = async () => {
     try {
-      setError(null)
-      setLoading(true)
       const response = await axios.request(options);
       setMovies(response.data.d);
-      setLoading(false)
     } catch (error) {
       console.error(error);
     }
   };
   const handleGetMoviesSlow = async () => {
     try {
-      setLoading(true);
       const { data } = await axios.get(
         "https://hub.dummyapis.com/delay?seconds=10"
       );
       setDelay(data);
       const response = await axios.request(options);
       setMovies(response.data.d);
-      setLoading(false);
-      setError(null)
     } catch (error:any) {
       console.error(error)
-      setError(error)
     }
   };
   const handleGetMoviesWrong = async () => {
     try {
-      setLoading(true)
       const response = await axios.request(optionsWrong);
       console.log(response.data);
       if(response.data.error) {
@@ -79,29 +69,14 @@ export function HomePage(props: IHomePageProps) {
         
       }
       setMovies(response.data.d);
-      setLoading(false)
     } catch (error:any) {
       console.error(error)
-      setError(error)
     }
   };
 
   useEffect(() => {
-    handleGetMovies();
+    handleGetMoviesWrong();
   }, []);
-
-  const getErrorView = () => {
-    // setLoading(false)
-    console.log("hi")
-    return (
-      <div>
-        Oh no! Something went wrong. {error? error.message : null}
-        <button onClick={handleGetMovies}>
-          Try again
-        </button>
-      </div>
-    )
-  }
 
   return (
     <Container>
@@ -109,19 +84,7 @@ export function HomePage(props: IHomePageProps) {
       <button onClick={handleGetMoviesWrong}>Wrong get</button>
       <button onClick={handleGetMovies}>Reload</button>
       <h1>Hot movies</h1>
-      {/* {!loading && movies.length > 0 ? (
-        movies.map((movie: any) => {
-          return (
-            <div key={movie.id}>
-              <img style={{ width: "200px" }} src={movie.i.imageUrl} alt="" />
-              <h2>{movie.l}</h2>
-            </div>
-          );
-        })
-      ) : (
-        <Spinner />
-      )} */}
-      {error? getErrorView() : !loading && movies.length > 0 ? (
+      { movies.length > 0 ? (
         movies.map((movie: any) => {
           return (
             <div key={movie.id}>
