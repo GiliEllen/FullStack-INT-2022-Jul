96fa49c164c35f1b803b97d03a20969ad9ebc5a8
Gili <gilimena95@gmail.com>
2023-06-08 17:11:24 +0300

    WIP

--- ReactNative/04-layout-hooks/debugdog/App.tsx
+++ ReactNative/04-layout-hooks/debugdog/App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
+  const [dogUrl, setDogUrl] = useState("");
+  const handleGetRandomDog = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://dog.ceo/api/breeds/image/random"
+      );
+      if (!data) throw new Error("no data");
+      setDogUrl(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetRandomDog()
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {dogUrl ? (
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: dogUrl }} />
+          <Text>Dog!</Text>
+        </View>
+      ) : (
+        <Text>No Dog</Text>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30, 
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    width: 300,
+    height: 300,
   },
 });
