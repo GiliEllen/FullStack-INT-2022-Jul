9987dfa4b7ec00b9be4fce1d7aebbf7b67b22b27
Gili <gilimena95@gmail.com>
2023-06-13 17:43:17 +0300

    WIP

--- ReactNative/06-notifications/example-class/App.tsx
+++ ReactNative/06-notifications/example-class/App.tsx
@@ -1,9 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 export default function App() {
+  const schedualNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "my first local",
+        body: "this is the body",
+        data: {userName: "Gili", id: 1}
+      },
+      trigger: {
+        seconds: 3,
+
+      },
+    });
+  };
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+
+        if (notification.request.content.data.id === 1)  {
+          console.log("got notification");
+          console.log(notification.request.content.data.userName);
+        } else {
+          console.log("this")
+        }
+        
+      }
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
+      <Button
+        title="Sechdual Notification"
+        onPress={schedualNotificationHandler}
+      ></Button>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
