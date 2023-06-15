b908b78bd0e3f265cbc77e74bdb37856c6bc48db
Gili <gilimena95@gmail.com>
2023-06-15 17:02:43 +0300

    WIP

--- React.js/99-practice/debugging/baseProject copy/src/components/index.tsx
+++ React.js/99-practice/debugging/baseProject copy/src/components/index.tsx
@@ -17,8 +17,6 @@ const Container = styled.div`
 export function HomePage(props: IHomePageProps) {
   const [movies, setMovies] = useState([]);
   const [delay, setDelay] = useState("");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
 
   const options = {
     method: "GET",
@@ -34,7 +32,7 @@ export function HomePage(props: IHomePageProps) {
   const optionsWrong = {
     method: "GET",
     // url: "https://imdb8.p.rapidapi.com/auto-complete",
-    url: "https://imdb8.p.rapidapi.com/auto-complete",
+    url: "https://imdb8.p.rapidapi.com/",
     params: { q: "game" },
     headers: {
       "X-RapidAPI-Key": "325d20f695msh54d1f03594ba21cp1c5242jsn69f27e1c4036",
@@ -52,34 +50,26 @@ export function HomePage(props: IHomePageProps) {
   };
   const handleGetMoviesSlow = async () => {
     try {
-      setLoading(true);
-      setError(null);
       const { data } = await axios.get(
         "https://hub.dummyapis.com/delay?seconds=10"
       );
       setDelay(data);
       const response = await axios.request(options);
-      if (response.data.error) {
-        throw new Error("E2");
-      }
       setMovies(response.data.d);
-      setLoading(false);
     } catch (error: any) {
-      setError(error);
+      console.error(error);
     }
   };
   const handleGetMoviesWrong = async () => {
     try {
-      setLoading(true);
       const response = await axios.request(optionsWrong);
+      console.log(response.data);
       if (response.data.error) {
         throw new Error("E1");
       }
-      setLoading(false);
       setMovies(response.data.d);
     } catch (error: any) {
-      setError(error);
-      setLoading(false);
+      console.error(error);
     }
   };
 
@@ -87,24 +77,13 @@ export function HomePage(props: IHomePageProps) {
     handleGetMoviesWrong();
   }, []);
 
-  const getErrorView = () => {
-    return (
-      <div>
-        somthing went wrong!
-        <button onClick={handleGetMovies}>Try again</button>
-      </div>
-    );
-  };
-
   return (
     <Container>
       <button onClick={handleGetMoviesSlow}>Slow get</button>
       <button onClick={handleGetMoviesWrong}>Wrong get</button>
       <button onClick={handleGetMoviesWrong}>Reload</button>
       <h1>Hot movies</h1>
-      {error ? (
-        getErrorView()
-      ) : !loading && movies.length > 0 ? (
+      {movies.length > 0 ? (
         movies.map((movie: any) => {
           return (
             <div key={movie.id}>
@@ -116,20 +95,6 @@ export function HomePage(props: IHomePageProps) {
       ) : (
         <Spinner />
       )}
-
-      {/* {movies.length > 0 ? (
-        movies.map((movie: any) => {
-          return (
-            <div key={movie.id}>
-              <img style={{ width: "200px" }} src={movie.i.imageUrl} alt="" />
-              <h2>{movie.l}</h2>
-            </div>
-          );
-        })
-      ) : (
-        <Spinner />
-      )}
-      {error ? getErrorView() : null} */}
     </Container>
   );
 }
