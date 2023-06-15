15fb6392961d00ec9040e960becdeee022e43229
Gili <gilimena95@gmail.com>
2023-06-15 18:39:37 +0300

    WIP

--- ReactNative/06-notifications/example-class/App.tsx
+++ ReactNative/06-notifications/example-class/App.tsx
@@ -1,8 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-
+import { Button, StyleSheet, Text, View, Alert } from "react-native";
+import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
   const schedualNotificationHandler = () => {
@@ -10,11 +20,10 @@ export default function App() {
       content: {
         title: "my first local",
         body: "this is the body",
-        data: {userName: "Gili", id: 1}
+        data: { userName: "Gili", id: 1 },
       },
       trigger: {
-        seconds: 3,
-
+        seconds: 1,
       },
     });
   };
@@ -22,20 +31,40 @@ export default function App() {
   useEffect(() => {
     const subscription = Notifications.addNotificationReceivedListener(
       (notification) => {
-
-        if (notification.request.content.data.id === 1)  {
+        if (notification.request.content.data.id === 1) {
           console.log("got notification");
           console.log(notification.request.content.data.userName);
         } else {
-          console.log("this")
+          console.log("this");
         }
-        
       }
     );
 
     return () => {
       subscription.remove();
     };
+  }, []);
+
+  useEffect(() => {
+    if (Device.isDevice) {
+      async function configurePushNotifications() {
+        const { status } = await Notifications.getPermissionsAsync();
+        let finalStatus = status;
+
+        if (finalStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+
+        if (finalStatus !== "granted") {
+          Alert.alert(
+            "Premission is required",
+            "push Notification need premission"
+          );
+        }
+        return;
+      }
+    }
   }, []);
 
   return (
