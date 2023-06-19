ffe157a70c15d6a17bae4dfc1423bd9ee6f57280
Gili <gilimena95@gmail.com>
2023-06-19 18:13:53 +0300

    WIP

--- ReactNative/07-camera/camera-example-class/App.tsx
+++ ReactNative/07-camera/camera-example-class/App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image, Alert } from "react-native";
 import { Camera } from "expo-camera";
 import { useEffect, useState, useRef } from "react";
 import * as Permissions from "expo-permissions";
+import MyButton from "./components/MyButton";
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
   // create interface for camera!
@@ -12,6 +14,7 @@ export default function App() {
   });
 
   const [image, setImage] = useState("");
+  const [hasMediaPremissions, setHasMediaPremissions] = useState<boolean>();
 
   let cameraRef = useRef<any>();
 
@@ -26,24 +29,76 @@ export default function App() {
     };
 
     askPermissions();
-  },[]);
+  }, []);
+
+  useEffect(() => {
+    const askPermissions = async () => {
+      const {status} = await MediaLibrary.requestPermissionsAsync()
+      setHasMediaPremissions(status === "granted")
+    } 
+    askPermissions()
+  },[])
 
   const takePicture = async () => {
     try {
       if (cameraRef) {
         const data = await cameraRef.current.takePictureAsync();
-        console.log(data)
-        setImage(data.uri)
-            }
+        console.log(data);
+        setImage(data.uri);
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    }
+  };
+
+  const retake = () => {
+    setImage("");
+  };
+
+  const saveImage = async () => {
+    if (image) {
+      try {
+        await MediaLibrary.createAssetAsync(image);
+        Alert.alert("Picture Saved!")
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={camera.type} ref={cameraRef}></Camera>
-      <Button title={"take Picture"} onPress={takePicture}/>
+      {image ? (
+        <Image source={{ uri: image }} style={styles.camera} />
+      ) : (
+        <Camera
+          style={styles.camera}
+          type={camera.type}
+          ref={cameraRef}
+        ></Camera>
+      )}
+
+      {image ? (
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <MyButton
+            title={"Retake Picture"}
+            icon={"retweet"}
+            onPress={retake}
+          />
+
+          <MyButton
+            title={"Save To Album"}
+            icon={"check"}
+            onPress={saveImage}
+          />
+        </View>
+      ) : (
+        <MyButton
+          title={"Take A Picture"}
+          icon={"camera"}
+          onPress={takePicture}
+        />
+      )}
     </View>
   );
 }
@@ -51,11 +106,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#000000",
     justifyContent: "center",
+    paddingBottom: 30,
   },
   camera: {
     flex: 1,
+    borderRadius: 20,
+  },
+  wrapper: {
+    // flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
   },
 });
--- ReactNative/07-camera/camera-example-class/components/MyButton.tsx
+++ ReactNative/07-camera/camera-example-class/components/MyButton.tsx
@@ -1,12 +1,29 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
+import { Entypo } from "@expo/vector-icons"
 
-const MyButton = ({ title, onPress }: any) => {
+const MyButton = ({ title, onPress, color, icon }: any) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{title}</Text>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Entypo name={icon} size={28} color={color ? color : "#f1f1f1"}/>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 export default MyButton;
+
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    flexDirection: "row",
+    alignItems:"center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#f1f1f1",
+    marginLeft:10
+  }
+})
