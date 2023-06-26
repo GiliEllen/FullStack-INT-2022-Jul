5c5d4a42f308005fd76d251d6fcf11c62b80b92f
Gili <gilimena95@gmail.com>
2023-06-26 17:13:25 +0300

    MUI + select

---
+++ React.js/14-mui/part1/myapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SelectCom from './components/SelectCom';
+
+function App() {
+  return (
+    <div className="App">
+      <SelectCom/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React.js/14-mui/part1/myapp/src/components/SelectCom.tsx
@@ -0,0 +1,54 @@
+import { Stack, Box, TextField, MenuItem } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+const SelectCom = () => {
+  const [lesson, setLesson] = useState<string>("");
+  const [lessons, setLessons] = useState<string[]>([]);
+
+  const handlechange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setLesson(ev.target.value);
+  };
+  const handlechangeArray = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const value = ev.target.value;
+    setLessons(typeof value === 'string' ? value.split(",") : value)
+  };
+
+  useEffect(() => {
+    console.log(lessons)
+  },[lessons])
+  return (
+<>
+      <Box mt={5} width={"250px"}>
+        <TextField
+          label="Select Class"
+          select
+          value={lesson}
+          onChange={handlechange}
+          fullWidth
+        >
+            <MenuItem value="PE">Physical education</MenuItem>
+            <MenuItem value="MATH">Math</MenuItem>
+            <MenuItem value="EN">English</MenuItem>
+        </TextField>
+      </Box>
+      <Box mt={5} width={"250px"}>
+        <TextField
+          label="Select Classes"
+          select
+          value={lessons}
+          onChange={handlechangeArray}
+          fullWidth
+          SelectProps={{multiple: true}}
+        >
+            <MenuItem value="PE">Physical education</MenuItem>
+            <MenuItem value="MATH">Math</MenuItem>
+            <MenuItem value="EN">English</MenuItem>
+        </TextField>
+      </Box>
+
+      </>
+
+  );
+};
+
+export default SelectCom;
