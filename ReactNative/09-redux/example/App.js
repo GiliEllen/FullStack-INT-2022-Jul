e192a76eb348a45cfb798efd9ac03c5e4d1afb7e
Gili <gilimena95@gmail.com>
2023-07-31 15:56:30 +0300

    WIP

---
+++ ReactNative/09-redux/example/App.js
@@ -0,0 +1,20 @@
+import { Provider } from "react-redux";
+import Home from "./src/screens/Home";
+import { store } from "./src/redux/store";
+import { Button } from "react-native";
+import { useState } from "react";
+import AnotherScreen from "./src/screens/AnotherScreen";
+
+export default function App() {
+  const [disp, setDisp] = useState(true);
+
+  const handleClick = () => {
+    setDisp(!disp)
+  }
+  return (
+    <Provider store={store}>
+      {disp ? <Home /> : <AnotherScreen />}
+      <Button onPress={handleClick} title={"switch"}></Button>
+    </Provider>
+  );
+}
---
+++ ReactNative/09-redux/example/src/screens/AnotherScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+
+const AnotherScreen = () => {
+    const count = useSelector((store) => store.count.count);
+  return (
+    <View style={styles.mt}>
+      <Text>AnotherScreen: {count}</Text>
+    </View>
+  )
+}
+
+export default AnotherScreen
+
+const styles = StyleSheet.create({
+    mt: {
+        marginTop: 50
+    }
+})
